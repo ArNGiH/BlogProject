8b111a9bfb550fd7ba5ac4e9cd2259d1b6f7837e
ArNGiH <aryannayar47@gmail.com>
2024-06-05 16:31:35 +0530

    Modified background color

--- megablog/src/App.jsx
+++ megablog/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import authService from './appwrite/auth'
 import {login,logout} from './store/authSlice'
 import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [loading,setLoading] = useState(true)
@@ -25,7 +26,7 @@ function App() {
   },[])
 
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'> 
+    <div className='min-h-screen flex flex-wrap content-between bg-white-400'> 
       <div className='w-full block'>
         <Header />
         <main>
